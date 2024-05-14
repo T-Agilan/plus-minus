22e6bf16099045f5d4d45b572ca7b39c6bca1654
Dharun297 <dharunpnb@gmail.com>
2024-05-14 14:51:22 +0530

    Firebase Authentication Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from "react";
+import "./App.css";
+import { MinusSquareOutlined, PlusOutlined } from "@ant-design/icons";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  getAuth,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+} from "firebase/auth";
+
+import { Button, Input } from "antd";
+import firebaseConfig from "./firebase";
+
+// Initialize Firebase outside the component
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
 
 function App() {
+  const [value, setValue] = useState(0);
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [confirmationResult, setConfirmationResult] = useState<any>(null);
+  const [appVerifier, setAppVerifier] = useState<RecaptchaVerifier | null>(
+    null
+  );
+  const [code, setCode] = useState<string | null>(null);
+  const generateOtp = useCallback(async () => {
+    try {
+      if (!appVerifier) {
+        const verifier = new RecaptchaVerifier(auth, "recaptcha-container", {});
+        console.log(verifier);
+        setAppVerifier(verifier);
+      }
+
+      if (appVerifier) {
+        const data = await signInWithPhoneNumber(
+          auth,
+          `+91 ${phoneNumber}`,
+          appVerifier
+        );
+        setConfirmationResult(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [auth, phoneNumber, appVerifier]);
+
+  const handleSubmit = async () => {
+    console.log(code, confirmationResult);
+    confirmationResult
+      ?.confirm(code)
+      .then((result: any) => {
+        // User signed in successfully.
+        console.log(result.user);
+        // ...
+      })
+      .catch((error: any) => {
+        // User couldn't sign in (bad verification code?)
+        // ...
+        console.log(error);
+      });
+  };
+  const onFinish = () => {
+    const provider = new GoogleAuthProvider();
+
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const increment = () => {
+    setValue(value + 1);
+  };
+
+  const decrement = () => {
+    setValue(value - 1);
+  };
+
+  useEffect(() => {
+    if (value > 5) return;
+    const timer = setTimeout(() => {
+      setValue(value + 1);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [value]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="icons">
+        <p>value:{value}</p>
+        <PlusOutlined onClick={increment} />
+        <MinusSquareOutlined onClick={decrement} />
+        <div>
+          <Button onClick={onFinish}>SignUp With Google</Button>
+        </div>
+        <div>
+          <p>Mobile Number</p>
+          <div>
+            <Input
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+            <div id="recaptcha-container"></div> {/* Recaptcha container */}
+            <Button onClick={generateOtp}>Generate OTP</Button>
+          </div>
+          <p>Enter Your OTP</p>
+          <Input
+            type="text"
+            // value={verificationCode} //verification code displayed after verify the I'm not robot.
+            onChange={(e) => setCode(e.target.value)}
+          />
+          <Button onClick={handleSubmit}>Verify OTP</Button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD4uZj1Vad2HzNUM0bsmwda03kKNvZy19c",
+    authDomain: "emyy-dev.firebaseapp.com",
+    projectId: "emyy-dev",
+    storageBucket: "emyy-dev.appspot.com",
+    messagingSenderId: "1094100429727",
+    appId: "1:1094100429727:web:052c3b2a96d37d5a5b2baa",
+    measurementId: "G-4TNQDXMBP8",
+    vapidKey:
+      "BABxWvZkrGQZ35nAiSmNyWP10_rKgotQLakptBfkR6a5sRn8mPqbTv3EVtcfhcxOk1d3vT2QKyV4eqxteAGzJ_U",
+  };
+  export default firebaseConfig;
