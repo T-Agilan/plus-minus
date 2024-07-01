812ad2396c483b8ec2f0185605f73e5a3a20bb3a
Dharun297 <dharunpnb@gmail.com>
2024-07-01 10:11:20 +0530

    api key updated

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCL5OS0HBUpkq6KHZBKqkcKzn9Is_6loqA",
-  authDomain: "email-login-production.firebaseapp.com",
-  projectId: "email-login-production",
-  storageBucket: "email-login-production.appspot.com",
-  messagingSenderId: "312899475235",
-  appId: "1:312899475235:web:13380a1706165052629e96"
-  };
-  export default firebaseConfig;
+  apiKey: "AIzaSyAdSHFRyh4hqTZEP3UZzYkU-h_E36IUeMc",
+  authDomain: "form-d74ae.firebaseapp.com",
+  projectId: "form-d74ae",
+  storageBucket: "form-d74ae.appspot.com",
+  messagingSenderId: "914943906971",
+  appId: "1:914943906971:web:4378e19b9ae509f3c2a23e",
+  measurementId: "G-LC7YEMSF66"
+};
+export default firebaseConfig;
